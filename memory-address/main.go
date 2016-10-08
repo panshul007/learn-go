97236a02a57d2a001364fa6d04ab44b4bffcfe67
Panshul Gupta <panshul007@gmail.com>
2016-10-08 22:22:29 +0200

    add: memory address.

---
+++ memory-address/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const metersToYards float64 = 1.09361
+
+// Scan -> always puts the entered values into the memory address.
+
+func main() {
+    a := 43
+    fmt.Println("a's memory address - ", &a)
+
+    var meters float64
+    fmt.Print("Enter meters swam: ")
+    fmt.Scan(&meters)
+    yards := meters * metersToYards
+    fmt.Println(meters, " meters is ", yards, " yards.")
+}
