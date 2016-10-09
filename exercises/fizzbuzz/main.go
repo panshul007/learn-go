6fb5b9b5c3b6efc2ae160670931cf8d02e02e76d
Panshul Gupta <panshul007@gmail.com>
2016-10-09 23:59:51 +0200

    add: some exercise - fizzbuzz.

---
+++ exercises/fizzbuzz/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    for i := 0; i <= 100; i++ {
+        if i % 15 == 0 {
+            fmt.Println(i, " -- fizzbuzz")
+        } else if i%3 == 0 {
+            fmt.Println(i, " -- buzz")
+        } else if i%5 == 0 {
+            fmt.Println(i, " -- fizz")
+        } else {
+            fmt.Println(i)
+        }
+    }
+}
