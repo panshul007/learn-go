c82a2cde381b64fe7ce1795da300c982719b242f
Panshul Gupta <panshul007@gmail.com>
2016-10-09 23:44:16 +0200

    add: if-else.

---
+++ if-else/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+    if true {
+        fmt.Println("This ran")
+    }
+
+    if false {
+        fmt.Println("This did not run")
+    }
+
+    if !true {
+        fmt.Println("This does not run")
+    }
+
+    b := true
+
+    if b {
+        fmt.Println("food")
+    }
+
+    if food := "Chocolate"; b {
+        fmt.Println(food)
+    }
+
+    if false {
+        fmt.Println("no dice")
+    } else {
+        fmt.Println("else works")
+    }
+
+    if false {
+        fmt.Println("no dice")
+    } else if true {
+        fmt.Println("else works")
+    } else {
+        fmt.Println("bla")
+    }
+}
