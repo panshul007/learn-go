d3ba2e70d88043c7cb95a87ca4e6ed745ad5f359
Panshul Gupta <panshul007@gmail.com>
2016-10-17 23:23:05 +0200

    add: arrays, passbyvalue, reference type

---
+++ arrays/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x [58] string
+    fmt.Println(x)
+    fmt.Println(len(x))
+    fmt.Println(x[42])
+
+    for i := 65; i <=122; i++ {
+        x[i-65] = string(i)
+    }
+
+    fmt.Println(x)
+    fmt.Println(len(x))
+    fmt.Println(x[42])
+    example2()
+}
+
+func example2() {
+    var x [256] int
+
+    fmt.Println(x)
+    fmt.Println(len(x))
+    fmt.Println(x[42])
+
+    for i := 0; i <256; i++ {
+        x[i] = i
+    }
+
+    // item, value
+    for i,v := range x {
+        fmt.Printf("%v - %T - %b\n", v, v, v)
+        if i > 50 {
+            break
+        }
+    }
+    //Todo: why do we need i in the second loop?
+}
